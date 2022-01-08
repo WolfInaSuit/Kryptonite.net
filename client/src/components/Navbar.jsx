e8cc0ef6681fefdfbb5ed70880a3c4e54648c025
WolfInaSuit <Stevferro1@gmail.com>
2022-01-08 02:35:43 -0500

    created lovely welcome section *

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,36 +20,45 @@ const Navbar = () => {
 
     return (
       <nav className="w-full flex md:justify-center justify-between items-center p-4">
+
         <div className="md:flex-[0.5] flex-initial justify-center items-center ">
+
             <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+
         </div>
+
         <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial ">
-           {["Market", "Exchange","Tutorials", "Wallets"].map((item, index) => (
+
+  {["Market", "Exchange","Tutorials", "Wallets"].map((item, index) => (
                 <NavbarItem key={item + index} title={item}  />
-           ))}
-           <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-               Login
-           </li>
+          ))}
+
+          <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">   
+            Login
+          </li>
         </ul>
+        
         <div className="flex relative">
             {toggleMenu 
-               ? <AiOutlineClose fontsize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false) } />
-                : <HiMenuAlt4 fontsize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
+              ? <AiOutlineClose fontsize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false) } />
+              : <HiMenuAlt4 fontsize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
                   }
                   {toggleMenu && (
                       <ul 
                         className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none
-                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
-                        "
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+                      
                       >
                           <li className="text-xl w-full my-2">
                             <AiOutlineClose OnClick={() => setToggleMenu(false)} />
                           </li>
-                           {["Market", "Exchange","Tutorials", "Wallets"].map((item, index) => (
+
+                          {["Market", "Exchange","Tutorials", "Wallets"].map((item, index) => (
                 <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
-           ))}
+            ))}
                       </ul>
                   )} 
+                  
         </div>
       </nav>
     );
